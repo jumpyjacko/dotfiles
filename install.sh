f54ccaa5963d2fe5d86dbb4b81bc13799e85574a
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2021-12-23 19:44:57 +1100

    Remove `paru` commands
    Unable to run `paru` commands as root

--- install.sh
+++ install.sh
@@ -2,13 +2,6 @@
 # Pacman installs
 pacman -Syu feh zsh neovim alacritty firefox exa git neofetch xorg
 
-# Paru install + what needs to be installed
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-cd ..
-paru -S nerd-fonts-mononoki
-
 # dwm install
 git clone https://github.com/JumpyJacko/dwm.git
 cd dwm
@@ -20,7 +13,7 @@ git clone https://github.com/thytom/dwmbar.git
 cd dwmbar
 ./install.sh
 cd ..
-sudo cp ~/.config/dwmbar/dwmbarrc /usr/share/dwmbar/config
+cp ~/.config/dwmbar/dwmbarrc /usr/share/dwmbar/config
 
 # Set zsh things
 chsh -s /usr/bin/zsh
