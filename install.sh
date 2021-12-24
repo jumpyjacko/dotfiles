f9e1500de1a2c603db45af84cc7611a87b8ae4f6
JumpyJacko <bobjacklee27@gmail.com>
2021-12-24 12:04:22 +1100

    Testing full install to gui

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+read -sp 'Sudoers Password: ' passvar
+
+# Pacman installs
+<<<<<<< HEAD
+echo passvar | sudo pacman -Syu feh zsh neovim alacritty firefox exa git neofetch xorg base-devel && sudo -k
+
+# Preparing the dotfiles repository
+git clone --bare https://github.com/JumpyJacko/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-di$HOME/.cfg --work-tree=$HOME'
+
+# Paru install + what needs to be installed
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+paru -S nerd-fonts-mononoki
+=======
+pacman -Syu feh zsh neovim alacritty firefox exa git neofetch xorg
+>>>>>>> c80c22bfc57752f31f6cf7de8b147b7ca18a5218
+
+# dwm install
+git clone https://github.com/JumpyJacko/dwm.git
+cd dwm
+echo passvar | sudo make clean install && sudo -k
+cd ..
+
+# dwmbar install and configuration
+git clone https://github.com/thytom/dwmbar.git
+cd dwmbar
+echo passvar | sudo ./install.sh
+cd ..
+cp .config/dwmbar/dwmbarrc /usr/share/dwmbar/config
+
+# Set zsh things
+chsh -s /usr/bin/zsh
+
+sudo -k
