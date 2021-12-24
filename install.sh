9347772f5e5f1db2b59a4393448e60e632b27457
JumpyJacko <bobjacklee27@gmail.com>
2021-12-24 11:59:47 +1100

    Testing full install to gui

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+read -sp 'Sudoers Password: ' passvar
+
 # Pacman installs
-pacman -Syu feh zsh neovim alacritty firefox exa git neofetch xorg base-devel
+echo passvar | sudo pacman -Syu feh zsh neovim alacritty firefox exa git neofetch xorg base-devel && sudo -k
 
 # Preparing the dotfiles repository
 git clone --bare https://github.com/JumpyJacko/dotfiles.git $HOME/.cfg
@@ -16,15 +19,17 @@ paru -S nerd-fonts-mononoki
 # dwm install
 git clone https://github.com/JumpyJacko/dwm.git
 cd dwm
-make clean install
+echo passvar | sudo make clean install && sudo -k
 cd ..
 
 # dwmbar install and configuration
 git clone https://github.com/thytom/dwmbar.git
 cd dwmbar
-./install.sh
+echo passvar | sudo ./install.sh
 cd ..
 sudo cp ~/.config/dwmbar/dwmbarrc /usr/share/dwmbar/config
 
 # Set zsh things
 chsh -s /usr/bin/zsh
+
+sudo -k
