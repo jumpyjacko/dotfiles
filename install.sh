910d46c93cfb53f5f14e087bbe98d149a9ffc8b7
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2021-12-24 12:11:38 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 read -sp 'Sudoers Password: ' passvar
 
 # Pacman installs
-echo $passvar | sudo pacman -Syu feh zsh neovim alacritty firefox exa git neofetch xorg base-devel && sudo -k
+echo $passvar | sudo -S pacman -Syu feh zsh neovim alacritty firefox exa git neofetch xorg base-devel && sudo -k
 
 # Preparing the dotfiles repository
 git clone --bare https://github.com/JumpyJacko/dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-di$HOME/.cfg --work-tree=$HOME'
+config checkout
 
 # Paru install + what needs to be installed
 git clone https://aur.archlinux.org/paru.git
@@ -19,13 +20,13 @@ paru -S nerd-fonts-mononoki
 # dwm install
 git clone https://github.com/JumpyJacko/dwm.git
 cd dwm
-echo $passvar | sudo make clean install && sudo -k
+echo $passvar | sudo -S make clean install && sudo -k
 cd ..
 
 # dwmbar install and configuration
 git clone https://github.com/thytom/dwmbar.git
 cd dwmbar
-echo $passvar | sudo ./install.sh
+echo $passvar | sudo -S ./install.sh
 cd ..
 cp .config/dwmbar/dwmbarrc /usr/share/dwmbar/config
 
