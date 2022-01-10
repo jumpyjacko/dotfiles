a8860c89c68dc6d6f7617757ce9a22f907d75f97
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2022-01-10 16:07:18 +1100

    Added echoes for each section
    Also diverted some Github pulls from https to ssh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ read -sp 'Github Email (For making a ssh key pair, be there for the setup): ' gi
 # Move to home directory, just to make sure you are already there
 cd ~
 
+echo "----------"
+echo "Downloading packages from the official arch repositories"
+echo "----------"
+
 # Pacman installs
 echo $passvar | sudo -S pacman -Syu --noconfirm feh zsh neovim alacritty firefox exa git openssh neofetch xorg python python-pip nodejs dmenu bpytop && sudo -k
 
@@ -19,12 +23,20 @@ echo $passvar | sudo -S pacman -Syu --noconfirm feh zsh neovim alacritty firefox
 # sudo pacman -S firefox
 # and pick number 2 when prompted to install noto-fonts instead of gnu-free-fonts.
 
+echo "----------"
+echo "Downloading and setting up dotfiles git bare repository"
+echo "----------"
+
 # Preparing the dotfiles repository
 git clone --bare git@github.com:JumpyJacko/dotfiles.git $HOME/.cfg
 /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME checkout
 
+echo "----------"
+echo "Downloading and install dwm and it's extras"
+echo "----------"
+
 # dwm install
-git clone https://github.com/JumpyJacko/dwm.git
+git clone git@github.com:JumpyJacko/dwm.git
 cd dwm
 echo $passvar | sudo -S make clean install && sudo -k
 cd ..
@@ -36,12 +48,24 @@ echo $passvar | sudo -S ./install.sh && sudo -k
 cd ..
 echo $passvar | sudo -S cp .config/dwmbar/dwmbarrc /usr/share/dwmbar/config && sudo -k
 
-# Download almond (fractal viewer)
+echo "----------"
+echo "Downloading Almonds"
+echo "----------"
+
+# Download almonds (fractal viewer)
 pip install pillow
 git clone https://github.com/Tenchi2xh/Almonds.git
 
+echo "----------"
+echo "Setting zsh as default shell"
+echo "----------"
+
 # Set zsh things
 echo $passvar | chsh -s /usr/bin/zsh
+
+echo "----------"
+echo "Downloading and installing paru as well as extra packages"
+echo "----------"
 
 # Paru install + what needs to be installed
 git clone https://aur.archlinux.org/paru.git
@@ -50,5 +74,11 @@ makepkg -si --noconfirm
 cd ..
 paru -Syu --noconfirm nerd-fonts-mononoki
 
+echo "----------"
+echo "Generating a ssh keypair for Github ssh"
+echo "----------"
+
 # Generating an ssh key pair for Github
 ssh-keygen -t ed25519 -C "$githubEmail"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
