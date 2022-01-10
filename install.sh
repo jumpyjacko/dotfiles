8b3d02d4aff597ae5623d85c72367df494db7826
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2022-01-10 16:27:58 +1100

    Fixed echoes

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 read -spe 'Sudoers Password (To reduce the amount of inputs to install): ' passvar
-read -pe '\nGithub Email (For making a ssh key pair, be there for the setup): ' githubEmail
+read -pe $'\nGithub Email (For making a ssh key pair, be there for the setup): ' githubEmail
 
 # Move to home directory, just to make sure you are already there
 cd ~
 
-echo -e "----------\n"
-echo -e "Downloading packages from the official arch repositories\n"
+echo -e "----------"
+echo -e "Downloading packages from the official arch repositories"
 echo -e "----------\n"
 
 # Pacman installs
@@ -23,16 +23,16 @@ echo $passvar | sudo -S pacman -Syu --noconfirm feh zsh neovim alacritty exa git
 # sudo pacman -S firefox
 # and pick number 2 when prompted to install noto-fonts instead of gnu-free-fonts.
 
-echo -e "----------\n"
-echo -e "Downloading and setting up dotfiles git bare repository\n"
+echo -e "----------"
+echo -e "Downloading and setting up dotfiles git bare repository"
 echo -e "----------\n"
 
 # Preparing the dotfiles repository
 git clone --bare git@github.com:JumpyJacko/dotfiles.git $HOME/.cfg
 /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME checkout
 
-echo -e "----------\n"
-echo -e "Downloading and install dwm and it's extras\n"
+echo -e "----------"
+echo -e "Downloading and install dwm and it's extras"
 echo -e "----------\n"
 
 # dwm install
@@ -48,23 +48,23 @@ echo $passvar | sudo -S ./install.sh && sudo -k
 cd ..
 echo $passvar | sudo -S cp .config/dwmbar/dwmbarrc /usr/share/dwmbar/config && sudo -k
 
-echo -e "----------\n"
-echo -e "Downloading Almonds\n"
+echo -e "----------"
+echo -e "Downloading Almonds"
 echo -e "----------\n"
 
 # Download almonds (fractal viewer)
 pip install pillow
 git clone https://github.com/Tenchi2xh/Almonds.git
 
-echo -e "----------\n"
-echo -e "Setting zsh as default shell\n"
+echo -e "----------"
+echo -e "Setting zsh as default shell"
 echo -e "----------\n"
 
 # Set zsh things
 echo $passvar | chsh -s /usr/bin/zsh
 
-echo -e "----------\n"
-echo -e "Downloading and installing paru as well as extra packages\n"
+echo -e "----------"
+echo -e "Downloading and installing paru as well as extra packages"
 echo -e "----------\n"
 
 # Paru install + what needs to be installed
@@ -74,8 +74,8 @@ makepkg -si --noconfirm
 cd ..
 paru -Syu --noconfirm nerd-fonts-mononoki
 
-echo -e "----------\n"
-echo -e "Generating a ssh keypair for Github ssh\n"
+echo -e "----------"
+echo -e "Generating a ssh keypair for Github ssh"
 echo -e "----------\n"
 
 # Generating an ssh key pair for Github
