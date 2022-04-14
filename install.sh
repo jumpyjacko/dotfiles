02d8a39b49b5e8d0692ab4d8715489575fbbba28
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2022-04-14 17:13:44 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This is mainly just for me (JumpyJacko) to be able to quickly set up a new
+# machine with my set-up.
+
 read -sp 'Sudoers Password (To reduce the amount of inputs to install): ' passvar
 
 # Move to home directory, just to make sure you are already there
@@ -10,12 +13,12 @@ echo -e "Downloading packages from the official arch repositories"
 echo -e "----------\n"
 
 # Pacman installs
-echo $passvar | sudo -S pacman -Syu --noconfirm feh zsh pamixer neovim alacritty exa git openssh neofetch xorg python python-pip nodejs dmenu bpytop && sudo -k
+echo $passvar | sudo -S pacman -Syu --noconfirm feh zsh pamixer neovim code alacritty exa git openssh neofetch xorg python python-pip nodejs dmenu && sudo -k
 # Pacman installs for Japanese IME, check for additional configuration on ArchWiki
 echo $passvar | sudo -S pacman -Syu --noconfirm fcitx5-mozc fcitx5-configtool fcitx5-qt fcitx5-gtk adobe-source-han-sans-jp-fonts && sudo -k
 
 # Note:
-# About using bpytop, braille characters will not render correctly due to
+# About using bpytop and other fancy sys-monitors, braille characters will not render correctly due to
 # firefox package using gnu-free-fonts.
 #
 # How to fix:
@@ -35,6 +38,9 @@ echo -e "----------\n"
 # Preparing the dotfiles repository
 git clone --bare https://github.com/JumpyJacko/dotfiles.git $HOME/.cfg
 /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME checkout
+
+# About the dotfiles:
+# Remember to set up the git ssh key after running this script
 
 echo -e "----------"
 echo -e "Downloading and install dwm and it's extras"
