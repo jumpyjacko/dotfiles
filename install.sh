b5c0284780c95502820df19f2f433fd6ffad57b0
JumpyJacko <bobjacklee27@gmail.com>
2022-08-23 18:05:51 +1000

    Changed Alacritty to Kitty
    Alacritty is dead, long live Kitty

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo -e "Downloading packages from the official arch repositories"
 echo -e "----------\n"
 
 # Pacman installs
-echo $passvar | sudo -S pacman -Syu --noconfirm feh zsh pamixer neovim code alacritty exa git openssh neofetch xorg dmenu && sudo -k
+echo $passvar | sudo -S pacman -Syu --noconfirm feh zsh pamixer neovim code kitty exa git openssh neofetch xorg dmenu && sudo -k
 # Pacman installs for bloat lmao
 echo $passvar | sudo -S pacman -Syu --noconfirm nodejs python python-pip gstreamer gst-plugin-pipewire gst-plugins-base gst-libav gst-plugins-good gst-plugins-bad
 # Pacman installs for Japanese IME, check for additional configuration on ArchWiki
